8fce4e8e351b7947825a3a3e4e85318c1fd8cae9
rahiminasab2015 <rahiminasab.e@husky.neu.edu>
2019-03-01 15:00:18 -0500

    book-donation solution added

--- solutions/book-donations++iter.py
+++ solutions/book-donations++iter.py
@@ -3,7 +3,8 @@ titles = input().strip().split(',')
 count = 0
 
 for title in titles:
-	if all(c in iter(title.lower()) for c in 'love'):
+	it = iter(title.lower())
+	if all(c in it for c in 'love'):
 		count += 1
 		
 print(len(titles) if count >= 10 else count)
