5c3dda2374787affa2969fb61144766a5e3e0946
rahiminasab2015 <rahiminasab.e@husky.neu.edu>
2019-03-09 12:04:29 -0500

    sum2list updated

--- solutions/session5/basics_sum2num.py
+++ solutions/session5/basics_sum2num.py
@@ -6,21 +6,19 @@ l3 = []
 if len(l2) > len(l1):
     l1, l2 = l2, l1
 
-i, carry = -1, 0
+carry =  0
 
-while -i <= len(l1) and -i <= len(l2):
-    d = l1[i] + l2[i] + carry
+for i in range(1, len(l2)+1):
+    d = l1[-i] + l2[-i] + carry
     l3.insert(0, d % 10)
     carry = d // 10
-    i -= 1
 
-while -i <= len(l1):
-    d = l1[i] + carry
+for i in range(len(l2)+1, len(l1)+1):
+    d = l1[-i] + carry
     l3.insert(0, d % 10)
     carry = d // 10
-    i -= 1
 
 if carry > 0:
     l3.insert(0, carry)
 
-print(l3)
+print(l3) 
